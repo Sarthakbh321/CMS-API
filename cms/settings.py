4a96ccdd46f2ea6a71fb90967dc728d4e72cc515
unknown-guy-1610 <rayanuthalas@gmail.com>
2019-12-02 19:02:57 +0530

    Heroku Trial 2

--- cms/settings.py
+++ cms/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -83,6 +85,10 @@ DATABASES = {
     }
 }
 
+DEBUG = True
+if os.environ.get("DATABASE_URL") is not None:
+    DATABASES['default'] = dj_database_url.config()
+    DEBUG = False
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
